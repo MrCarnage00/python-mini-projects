8ee97ea3246f385f029badb2c612d93ed7b2d00f
Priyanshu Sharma <priyanshushr133@gmail.com>
2026-06-09 22:20:18 +0530

    Add Day 09 Silent Auction project

--- Day-09-Silent-Auction/main.py
+++ Day-09-Silent-Auction/main.py
@@ -1,1 +1,24 @@
+import art
+print(art.hammer)
 
+bids = {}
+choice = True
+def bidding():
+    print("Welcome to the Silent Auction!")
+    name = input("What is your name? ")
+    bid = int(input("Place your bid : $"))
+    bids[name] = bid
+
+
+while choice :
+    bidding()
+    again = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+    if again == "no":
+        choice = False
+    elif again not in ["yes" , "no"]:
+        print("Error!")
+        break
+    print("\n" * 100)
+
+winner = max(bids,key = bids.get)
+print(f"The winner is {winner} with the highest bid of ${bids[winner]}")
